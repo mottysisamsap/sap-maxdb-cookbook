49bca1232374308e5ebd9d762927370174f1e1a9
Haggai Philip Zagury <hagzag@hagzag.com>
2016-01-20 08:47:15 +0200

    Update version name and license

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name             'maxdb'
 maintainer       'Stefan Weidemüller-Schmidt'
 maintainer_email 'stefan.weidemueller-schmidt@sap.com'
-license          'SAP AG, All rights reserved'
+license          "Apache 2.0"
 description      'Installs/Configures maxdb'
 #long_description <<-TEXT
-version          '0.1.13'
+version          '0.1.14'
 
 depends 'ark', "0.4.0" 
 depends 'lvm'
+%w{ suse }.each do |os|
+  supports os
+end
